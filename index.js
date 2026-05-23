fc080a5bd1bd7beeefe99be06361413cd8a9d149
rahulinfolabz <rahulinfolabz@gmail.com>
2026-05-23 13:44:13 +0530

    index

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ app.use(cors({
     "http://localhost:3001",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://your-frontend.onrender.com", // ← replace with your actual frontend URL
+    "https://your-frontend.onrender.com",
+    "https://front-project-r0xq.onrender.com" // ← replace with your actual frontend URL
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
